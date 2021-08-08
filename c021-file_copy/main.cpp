da5fe751c3ef753fddfe6d7108235b8b8fe0a63b
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2021-08-08 22:30:44 +0100

    Initial Commit

---
+++ c021-file_copy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c021-file_copy
+//
+//  Created by Mathieu Delehaye on 08/08/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
